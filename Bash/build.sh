728ddf733f6607904d21561d03ef3df868836cab
Hussi503 <86091094+Hussi503@users.noreply.github.com>
2025-04-09 17:05:54 +0530

    Update build.sh

--- Bash/build.sh
+++ Bash/build.sh
@@ -3,8 +3,11 @@ sudo docker rmi -f $(sudo docker images -q) ##this is not recommned step, i am d
 sudo rm -r gold ## these steps are not recommened instead you can modify script as shown below
 sudo mkdir gold
 cd gold/
-sudo git clone https://github.com/sagarkakkalasworld/Day4-code.git
+sudo git clone https://github.com/React-app-demo/Day4-code.git
 cd Day4-code/
 sudo docker build -t react-microk8s -f golddockerfile .
-sudo docker tag react-microk8s:latest sagarkakkalasworld/react-microk8s:latest ##make sure you did docker login
-sudo docker push sagarkakkalasworld/react-microk8s:latest
+# sudo docker tag react-microk8s:latest sagarkakkalasworld/react-microk8s:latest ##make sure you did docker login
+# sudo docker push sagarkakkalasworld/react-microk8s:latest
+
+sudo docker tag react-docker:latest hussain503/react-docer-hub:latest
+sudo docker push hussain503/react-docer-hub:latest
